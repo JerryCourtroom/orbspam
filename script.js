75feda57a75bc7b2a2c6cf296eb1e536a563dd5e
JerryCourtroom <jerrycourtroom123@gmail.com>
2025-06-21 12:47:19 -0400

    Commit 3 - Added comments within code and created a countdown timer; ongoing orb investigation bug

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*
+    Orb Spam (JavaScript Game)
+    Creation Date: 6.13.25
+    Author: JerryCourtroom
+*/
+
+//Note: Always use script tag to link index.html with script.js!
 var tabTitle = document.getElementById("tabTitle");
 var home = document.getElementById("home");
 var numOfOrbsElement = document.getElementById("numOfOrbsElement");
@@ -5,15 +12,17 @@ var firstOrbMessage = document.getElementById("firstOrbMessage");
 var strangeOrbMessage = document.getElementById("strangeOrbMessage");
 var investigateFirstOrbButton = document.getElementById("investigateFirstOrb");
 var investigateOrbCooldownMessage = document.getElementById("investigateOrbCooldownMessage");
+var investigateOrbCooldownTimer = setTimeout(investigateOrbCooldownControl, 1000);
 var numOfOrbs = 0;
-var firstOrbMilestone = 100;
-var reachedFirstOrbMilestone = false;
 var orbsPerClick = 1;
-var obtainedFirstOrb = false;
+var firstOrbMilestone = 100;
 var investigateOrbCooldown = 10;
+var reachedFirstOrbMilestone = false;
+var obtainedFirstOrb = false;
 var investigateOrbCooldownFinished = false;
-var timeUp = 0;
 
+//displays first orb congratulation message to user only once
+//user obtains strange orb on the 100th orb collected
 function getOrb() {
 
     numOfOrbs += 1;
@@ -26,9 +35,7 @@ function getOrb() {
         obtainedFirstOrb = true;
     }
     if (numOfOrbs == firstOrbMilestone) {
-
         reachedFirstOrbMilestone = true;
-
     }
     if (reachedFirstOrbMilestone) {
         foundStrangeOrb();
@@ -36,12 +43,14 @@ function getOrb() {
 
 }
 
+//hides first orb message 1 second after displaying
 function hideFirstOrbMessage() {
 
     firstOrbMessage.style.display = "none";
 
 }
 
+//displays strange orb message and first orb investigation button
 function foundStrangeOrb() {
 
     strangeOrbMessage.innerHTML = "You found a strange orb. Investigate?";
@@ -50,13 +59,14 @@ function foundStrangeOrb() {
 
 }
 
+//first orb investigation costs 99 orbs and investigation cooldown starts
+//after investigation, user finds vision orb
 function investigateFirstOrb() {
 
     numOfOrbs -= 99;
     numOfOrbsElement.innerHTML = "Orbs: " + numOfOrbs;
-    investigateOrbCooldownMessage.style.display = "block";
-    while (investigateOrbCooldown != timeUp) {
-        setInterval(investigateOrbCooldownControl, 10000);
+    for (var i = 0; i < 10; i++) {
+        investigateOrbCooldownTimer;
     }
     investigateOrbCooldownFinished = true;
     if (investigateOrbCooldownFinished) {
@@ -65,19 +75,24 @@ function investigateFirstOrb() {
 
 }
 
+//first orb investigation time takes 10 seconds
 function investigateOrbCooldownControl() {
 
     investigateOrbCooldownMessage.innerHTML = "The investigation costed 99 orbs. Time until finished: " + investigateOrbCooldown;
+    investigateOrbCooldownMessage.style.display = "block";
     investigateOrbCooldown -= 1;
 
 }
 
+//save and load feature (refresh doesn't restart game)
 function saveGame() {
 
     localStorage.setItem("numOfOrbs", numOfOrbs);
     localStorage.setItem("orbsPerClick", orbsPerClick);
+    localStorage.setItem("investigateOrbCooldown", investigateOrbCooldown);
     localStorage.setItem("reachedFirstOrbMilestone", reachedFirstOrbMilestone);
     localStorage.setItem("obtainedFirstOrb", obtainedFirstOrb);
+    localStorage.setItem("investigateOrbCooldownFinished", investigateOrbCooldownFinished);
 
     requestAnimationFrame(saveGame);
 
@@ -101,6 +116,7 @@ function loadGame() {
 
 }
 
+//prevents localStorage from breaking when new user plays
 if (localStorage.getItem("numOfOrbs") == null) {
     saveGame();
     loadGame();
